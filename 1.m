d9e9d1cdfc2366b0a48d03078df764c6fa66fa2c
rtanmay <rtanmay05@gmail.com>
2018-01-24 08:55:31 +0530

    Added new files

--- 1.m
+++
@@ -1,5 +0,0 @@
-start="going to start on this project soon!"
-a=1
-b=2
-z=26
-sum=a+b
---
+++ Octave.m
@@ -0,0 +1,6 @@
+//This program is written in octave so may NOT work correctly in MATLAB 
+start="going to start on this project soon!"
+a=1
+b=2
+z=26
+sum=a+b
